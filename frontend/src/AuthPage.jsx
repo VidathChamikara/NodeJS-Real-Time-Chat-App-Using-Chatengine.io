4a43ae08a9142db953434e0f3070722f906427e1
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-04-13 15:14:04 +0530

    import npm react chat engine modules and used

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,8 +1,15 @@
+import axios from 'axios'
+
 const AuthPage = (props) => {
     const onSubmit = (e) => {
       e.preventDefault();
       const { value } = e.target[0];
-      props.onAuth({ username: value, secret: value });
+      axios.post(
+        'http://localhost:3001/authenticate',
+        {username: value}
+        )
+        .then(r => props.onAuth({...r.data, secret: value}))
+        .catch(e => console.log('error', e))      
     };
   
     return (
--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,4 +1,20 @@
-const ChatsPage = ( )=> {
-    return<>Chats...</>
-}
-export default ChatsPage
+import {
+  MultiChatSocket,
+  MultiChatWindow,
+  useMultiChatLogic,
+} from "react-chat-engine-advanced";
+
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "07557c21-e16a-4987-aaa1-50405a7d5883",
+    props.user.username,
+    props.user.secert
+  );
+  return (
+    <div style={{ height: "100vh" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+    </div>
+  );
+};
+export default ChatsPage;
