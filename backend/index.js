ee29a2e96bc3648d1bc54aa612082239acb728ae
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-04-13 11:37:43 +0530

    Develop http put request and connect with chatengine.io

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 const app = express();
 app.use(express.json());
@@ -8,11 +9,16 @@ app.use(cors({ origin: true }));
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
   try{
-
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      {username: username, secret: username, first_name: username},
+      {headers: {"private-key":"934ad999-3bae-4009-b753-81f276aac549"}}
+    )
+    return res.status(r.status).json(r.data)
   }catch(e){
-    
+    return res.status(e.response.status).json(e.response.data)
   }
-  934ad999-3bae-4009-b753-81f276aac549
+  
   return res.json({ username: username, secret: "sha256..." });
 });
 
